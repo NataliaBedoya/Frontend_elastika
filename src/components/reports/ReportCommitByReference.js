c2eefff9c84b337fdd2f292aaefba159db4d4ede
Natalia <nbedoyaruiz@gmail.com>
2021-08-22 18:02:24 -0500

    estilos de la tabla commit by reference

--- src/components/reports/ReportCommitByReference.js
+++ src/components/reports/ReportCommitByReference.js
@@ -9,8 +9,6 @@ function CommitByMaterial() {
     };
   });
 
-  //console.log(commitByMaterial);
-
   const commitToShow = commitByMaterial.filter(
     (commit) => commit.material._id === materialToGetReport
   );
@@ -22,13 +20,17 @@ function CommitByMaterial() {
       commitToShow.map((commit) => {
         return (
           <tr>
-            <td>
+            <td style={{ width: "30%", textAlign: "left" }}>
               {commit.customer.name} <br />
               Order: {commit.order}
             </td>
-            <td>{new Intl.NumberFormat().format(parseInt(commit.amount))}</td>
-            <td>{new Date(commit.deliveryDate).toDateString()}</td>
-            <td>{commit.notes}</td>
+            <td style={{ width: "20%", textAlign: "center" }}>
+              {new Intl.NumberFormat().format(parseInt(commit.amount))}
+            </td>
+            <td style={{ width: "25%", textAlign: "center" }}>
+              {new Date(commit.deliveryDate).toDateString()}
+            </td>
+            <td style={{ width: "25%", textAlign: "left" }}>{commit.notes}</td>
           </tr>
         );
       })
@@ -41,10 +43,10 @@ function CommitByMaterial() {
         <caption>Commited Product</caption>
         <thead>
           <tr>
-            <th>Customer</th>
-            <th>Amount (kg) </th>
-            <th>Delivery Date</th>
-            <th>Notes</th>
+            <th style={{ width: "30%", textAlign: "center" }}>Customer</th>
+            <th style={{ width: "20%", textAlign: "center" }}>Amount (kg) </th>
+            <th style={{ width: "25%", textAlign: "center" }}>Delivery Date</th>
+            <th style={{ width: "25%", textAlign: "center" }}>Notes</th>
           </tr>
         </thead>
         <tbody>{renderTable()}</tbody>
